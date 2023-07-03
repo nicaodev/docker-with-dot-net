4ff5732699865dda36ca0cd7152784a97358638f
nicaodev <alexandre.nicos@gmail.com>
2023-07-03 20:16:48 -0300

    docker-compose init

--- dockerContainer/dockerContainer/Model/PopulaDb.cs
+++ dockerContainer/dockerContainer/Model/PopulaDb.cs
@@ -25,13 +25,18 @@ public static class PopulaDb
             context.testeDockers.AddRange(
 
                 new TesteDockerDto("Testando Volumes com Docker", "", "Nicolas Alexandre"),
-                new TesteDockerDto("Primeiro - Criar Volume", "docker volume create dadosdb", "Nicolas Alexandre"),
-                new TesteDockerDto("Segundo - Cria imagem docker", "docker image pull mysql:5.7", "Nicolas Alexandre"),
-                new TesteDockerDto("Terceiro - Inspeciona (checar qual pasta usa-se para Volume que a imagem do container)", "docker image inspect mysql:5.7", "Nicolas Alexandre"),
-                new TesteDockerDto("Quarto - Criar Container a partir da imagem", "docker container run -d --name mysql -v dadosdb:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=159753 -e bind-addres=0.0.0.0 mysql:5.7", "Nicolas Alexandre"),
-                new TesteDockerDto("Quinta - Inspecionar a rede virtual que o docker deu para conexão (pegar ip e setar em DBHOST)", "docker network inspect bridge", "Nicolas Alexandre"),
-                new TesteDockerDto("Sexto - Criar container c base na imagem (controller) e start automatico na migration", "docker container run -d --name appmigrations -p 3000:80 -e DBHOST=172.17.0.2 testedockersql", "Nicolas Alexandre"),
-                new TesteDockerDto("Setimo - Usando Docker Compose", "docker compose", "Nicolas Alexandre")
+                new TesteDockerDto("1 - Criar Volume", "docker volume create dadosdb", "Nicolas Alexandre"),
+                new TesteDockerDto("2 - Cria imagem docker", "docker image pull mysql:5.7", "Nicolas Alexandre"),
+                new TesteDockerDto("3 - Inspeciona (checar qual pasta usa-se para Volume que a imagem do container)", "docker image inspect mysql:5.7", "Nicolas Alexandre"),
+                new TesteDockerDto("4 - Criar Container a partir da imagem", "docker container run -d --name mysql -v dadosdb:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=159753 -e bind-addres=0.0.0.0 mysql:5.7", "Nicolas Alexandre"),
+                new TesteDockerDto("5 - Inspecionar a rede virtual que o docker deu para conexão (pegar ip e setar em DBHOST)", "docker network inspect bridge", "Nicolas Alexandre"),
+                new TesteDockerDto("6 - Criar container c base na imagem (controller) e start automatico na migration", "docker container run -d --name appmigrations -p 3000:80 -e DBHOST=172.17.0.2 testedockersql", "Nicolas Alexandre"),
+                new TesteDockerDto("7 - Usando Docker Compose", "docker compose", "Nicolas Alexandre"),
+                new TesteDockerDto("8 - Docker compose", "____________", "Nicolas Alexandre"),
+                new TesteDockerDto("9 - Verificando sintaxe do arquivo.yml", "docker-compose build", "Nicolas Alexandre"),
+                new TesteDockerDto("10 - Fazendo teste com arquivo.yml (Processa arquivo e inicia aplicação)", "docker-compose up -d", "Nicolas Alexandre"),
+                new TesteDockerDto("10 - Remove containers e redes descritas no arquivo.yml", "docker-compose down -v", "Nicolas Alexandre"),
+                new TesteDockerDto("11 - Builder final Docker Compose", "docker-compose -f docker-compose.yml build ", "Nicolas Alexandre")
                 );
 
             context.SaveChanges();
